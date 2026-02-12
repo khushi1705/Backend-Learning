2f52528ade71986841915cef1473b9005ca7b93c
khushi1705 <khusighimire66@gmail.com>
2026-02-12 14:19:54 +0545

    learning express dependecncy

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("hello world");
+
+
+
+
+
+const express = require(`express`);
+const app = express();
+
+const PORT = 5000;
+
+
+app.listen(PORT,() => {
+console.log(`Server is running on PORT${PORT} `)
+
+} );
+
+app.get("/",(req, res) => {
+
+res.send("Salman Khan")
+});
+console.log("hello world")
